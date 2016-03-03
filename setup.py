1242da9278ccadc798ea9a22015972a6066a71a2
Jeffrey Ouyang <jeffreyouyang5293@gmail.com>
2016-03-02 22:51:27 -0800

    Initial deployment of this app to the web

--- setup.py
+++ setup.py
@@ -6,5 +6,4 @@ setup(name='Dybb Stock Simulator',
       author='Jeffrey Ouyang',
       author_email='jeffreyouyang5293@gmail.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask','flask-wtf','flask-babel','markdown','flup',
-      )
+      install_requires=['Flask', 'flask-wtf', 'flask-babel', 'markdown', 'flup'])
